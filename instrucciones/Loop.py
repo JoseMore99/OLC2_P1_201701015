307b230551b93cf6f8eb55d22ddc3b1a1b52cb9b
JoseMore99 <57239404+JoseMore99@users.noreply.github.com>
2022-10-18 21:54:04 -0600

    C3D loop

--- instrucciones/Loop.py
+++ instrucciones/Loop.py
@@ -2,6 +2,7 @@ from expresion.Tipo import Tipo
 from instrucciones.instrucciones import instrucciones
 from simbolo.ambito import ambito
 from simbolo.arbol import Arbol
+from simbolo.listasimboloc3d import listasimboloc3d
 
 class Loop(instrucciones):
     
@@ -22,4 +23,35 @@ class Loop(instrucciones):
                 if(respuesta["tipo"]=="201701015R"):
                         return respuesta
     def traducir(self,arbol:Arbol, tabla):
-        pass
+        codigo = ""
+        lControl = arbol.newLabel()
+        lVerdadero = arbol.newLabel()
+        lFalso = arbol.newLabel()
+        nuevaTabla = listasimboloc3d(tabla)
+        nuevaTabla.setNombre('Loop')
+        arbol.tamReturn += tabla.tamanio
+        codigo += arbol.masStackV(tabla.tamanio)
+        codigo += arbol.getLabel(lControl)
+        codigo += arbol.getCond2("1.0", "==", "1.0", lVerdadero)
+        codigo += arbol.goto(lFalso)
+        codigo += arbol.getLabel(lVerdadero)
+        aux = ""
+        tip = Tipo.ENTERO
+        transferencia = {"break":lFalso,"continue":lControl,"return":self.eReturn(),"temporal":self.eTemporal()}
+        aux = self.contenido.traducir(arbol, nuevaTabla,condi=transferencia)
+
+        arbol.tamReturn -= tabla.tamanio
+        codigo += aux["codigo"]
+        codigo += arbol.goto(lControl)
+        codigo += arbol.getLabel(lFalso)
+        '''
+        LC:
+        if a>b goto LV
+        goto LF
+        LV: 
+        instrucciones
+        goto LC
+        LF:
+        '''
+        codigo += arbol.menosStackV(tabla.tamanio)
+        return {'temporal': "", 'codigo': codigo, 'tipo': tip}
