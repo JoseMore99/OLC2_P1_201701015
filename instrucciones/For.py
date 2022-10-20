6175ae85c23da824045dcb16abf51ba48927e9f5
JoseMore99 <57239404+JoseMore99@users.noreply.github.com>
2022-10-19 22:36:10 -0600

    C3D for basico

--- instrucciones/For.py
+++ instrucciones/For.py
@@ -1,8 +1,10 @@
 from expresion.Tipo import Tipo
 from expresion.nativo import nativo
+from instrucciones.declarar import declarar
 from instrucciones.instrucciones import instrucciones
 from simbolo.ambito import ambito
 from simbolo.arbol import Arbol
+from simbolo.listasimboloc3d import listasimboloc3d
 from simbolo.simbolo import simbolo
 
 class For(instrucciones):
@@ -61,4 +63,70 @@ class For(instrucciones):
                 errores.Errores.nuevoError(self.fila,self.comlumna, 'Semantico', "Expresiones en for no admitidas")
     
     def traducir(self,arbol:Arbol, tabla):
-        pass
+        tip = Tipo.ENTERO
+        codigo = ""
+        tempControl = arbol.newTemp()
+        lControl = arbol.newLabel()
+
+        lFalso = arbol.newLabel()
+        lSalida = arbol.newLabel()
+        nuevaTabla = listasimboloc3d(tabla)
+        nuevaTabla.setNombre('For')
+        arbol.tamReturn += tabla.tamanio
+        codigo += arbol.masStackV(tabla.tamanio)
+        dec = self.variable
+        dec.tipo = self.varcambio.tipo
+        nuevaDec = dec.traducir(arbol, nuevaTabla)
+        if self.varcambio2 != self.varcambio:
+            val1 = self.varcambio.traducir(arbol, nuevaTabla)
+            val2 = self.varcambio2.traducir(arbol, nuevaTabla)
+            codigo += val1["codigo"]
+            codigo += val2["codigo"]
+            #print(val1, val2)
+            codigo += arbol.assigTemp1(
+                tempControl["temporal"], val1["temporal"])
+            codigo += arbol.getLabel(lControl)
+            codigo += arbol.getCond2(tempControl["temporal"],">=", val2["temporal"], lSalida)
+            codigo += arbol.assigStackN("P", tempControl["temporal"])
+            codigo += arbol.goto(lFalso)
+            codigo += arbol.getLabel(lFalso)
+            # Instrucciones
+            transferencia = {"break":lSalida,"continue":lControl,"return":self.eReturn(),"temporal":self.eTemporal()}
+            aux = self.contenido.traducir(arbol, nuevaTabla,condi=transferencia)
+
+            arbol.tamReturn -= tabla.tamanio
+            codigo += aux["codigo"]
+            codigo += arbol.assigTemp2(tempControl["temporal"], tempControl["temporal"], "+", "1.0")
+            codigo += arbol.goto(lControl)
+        codigo += arbol.getLabel(lSalida)
+        arbol.tamReturn -= tabla.tamanio
+        codigo += arbol.menosStackV(tabla.tamanio)
+        return {'temporal': '', 'codigo': codigo, 'tipo': tip}
+
+
+'''codigo = ""
+        tempC = arbol.newTemp()
+        tempF = arbol.newTemp()
+        lControl = arbol.newLabel()
+        lFalso = arbol.newLabel()
+        nuevaTabla = listasimboloc3d(tabla)
+        nuevaTabla.setNombre('For')
+        codigo += arbol.masStackV(tabla.tamanio)
+        
+        if self.varcambio != self.varcambio2:
+            temp1 = self.varcambio.traducir(arbol,tabla)
+            codigo+= temp1["codigo"]
+            codigo += arbol.assigTemp1(tempC,temp1["temporal"])
+            temp2 = self.varcambio2.traducir(arbol,tabla)
+            codigo+= temp2["codigo"]
+            codigo += arbol.assigTemp1(tempF,temp2["temporal"])
+            codigo += arbol.getLabel(lControl)
+            transferencia = {"break":lFalso,"continue":lControl,"return":self.eReturn(),"temporal":self.eTemporal()}
+            aux = self.contenido.traducir(arbol, nuevaTabla,condi=transferencia)
+            codigo += aux["codigo"]
+            codigo += arbol.getCond2(tempC["temporal"], "==",tempF["temporal"], lFalso)
+            codigo += arbol.assigTemp2(tempC["temporal"],tempC["temporal"], " + ", "1.0")
+            codigo += arbol.goto(lControl)
+            codigo += arbol.getLabel(lFalso)
+
+        return {'codigo': codigo}'''
